82f314485bd730fec0d07a9656c3e0f9f0607b37
akash@maticui <akash@maticui.com>
2024-09-03 14:46:34 +0530

    route update

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -2,7 +2,7 @@ const router = require("express").Router()
 const auth = require("./../controllers/auth.controller")
 
 router
-    .post("/register", auth.registerUser)
+    // .post("/register", auth.registerUser)
     .post("/login", auth.loginUser)
     .post("/logout", auth.logoutUser)
 module.exports = router
