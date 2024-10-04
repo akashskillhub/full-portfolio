cefbb4bdd506bf8401e98b111ec98eef54a3f41a
akash@maticui <akash@maticui.com>
2024-10-04 13:31:56 +0530

    rate limit fix

--- routes/public.routes.js
+++ routes/public.routes.js
@@ -1,9 +1,10 @@
 const router = require("express").Router()
+const { rateLimit } = require("express-rate-limit")
 const public = require("./../controllers/public.controller")
 
 router
     .get("/get-carousel", public.getCarousel)
     .get("/get-projects", public.getProjects)
     .get("/get-project-details/:id", public.getProjectDetails)
-    .post("/add-enquery", public.AddEnqueryMessage)
+    .post("/add-enquery", rateLimit({ windowMs: 15 * 60 * 1000, limit: 1 }), public.AddEnqueryMessage)
 module.exports = router
